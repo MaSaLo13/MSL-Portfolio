8adcc845544471617f5c021af640520c4e0ea4f8
marvinsantiago <marvinsantiago@MindYouBusiness.lan>
2022-08-23 20:40:11 -0700

    background color, added more projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,21 +3,21 @@ import React from 'react';
 export default function Footer() {
 
     return (
-<footer class="bg-dark text-center text-white">
+<footer className="bg-dark text-center text-white">
 
-  <div class="container p-4 pb-0">
+  <div className="container p-4 pb-0">
 
-    <section class="mb-4">
-      <a class="btn btn-outline-light btn-floating m-1" href="https://linkedin.com/in/marvin-santiagolopez" role="button"
-        ><i class="fab fa-linkedin-in"></i></a>
+    <section className="mb-4">
+      <a className="btn btn-outline-light btn-floating m-1" href="https://linkedin.com/in/marvin-santiagolopez" role="button"
+        ><i className="fab fa-linkedin-in"></i></a>
 
-      <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/MaSaLo13" role="button"
-        ><i class="fab fa-github"></i></a>
+      <a className="btn btn-outline-light btn-floating m-1" href="https://github.com/MaSaLo13" role="button"
+        ><i className="fab fa-github"></i></a>
     </section>
   </div>
 
-  <div class="text-center p-3"  >
-    <a class="text-white" href="https://mdbootstrap.com/">Marvin Santiago Lopez</a>
+  <div className="text-center p-3"  >
+    <p className="text-white"> Marvin Santiago Lopez</p>
   </div>
 </footer>
 );
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -3,50 +3,50 @@ import React from 'react';
 
 export default function Portfolio() {
   return (
-    <div class="row row-cols-1 row-cols-md-2 g-4">
-  <div class="col">
-    <div class="card">
-      <img src="https://github.com/MaSaLo13/Weather-Dash/raw/main/assets/img/first.png" class="card-img-top" alt="Hollywood Sign on The Hill"/>
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
+    <div className="row row-cols-1 row-cols-md-2 g-4">
+  <div className="col">
+    <div className="card">
+      <img src="https://github.com/MaSaLo13/Weather-Dash/raw/main/assets/img/first.png" className="card-img-top" alt="Hollywood Sign on The Hill"/>
+      <div className="card-body">
+        <h5 className="card-title">Weather-Dash</h5>
+        <p className="card-text">
           This is a longer card with supporting text below as a natural lead-in to
           additional content. This content is a little bit longer.
         </p>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
-      <img src="https://github.com/GSiggins/ingredients/raw/main/assets/images/landing.png" class="card-img-top" alt="Palm Springs Road"/>
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
+  <div className="col">
+    <div className="card">
+      <img src="https://github.com/GSiggins/ingredients/raw/main/assets/images/landing.png" className="card-img-top" alt="Palm Springs Road"/>
+      <div className="card-body">
+        <h5 className="card-title">Chef Buddy</h5>
+        <p className="card-text">
           This is a longer card with supporting text below as a natural lead-in to
           additional content. This content is a little bit longer.
         </p>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
-      <img src="https://github.com/oconnorjohnson/cream-finder/raw/main/public/images/logo.png" class="card-img-top" alt="Los Angeles Skyscrapers"/>
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-        <a class="btn btn-outline-dark btn-floating m-1" href="https://github.com/oconnorjohnson/cream-finder" role="button"
-        ><i class="fab fa-github"></i></a>
-        <a class="btn btn-outline-dark btn-floating m-1" href="https://enthousiaste-monsieur-80020.herokuapp.com/" role="button"
+  <div className="col">
+    <div className="card">
+      <img src="https://github.com/oconnorjohnson/cream-finder/raw/main/public/images/logo.png" className="card-img-top" alt="Los Angeles Skyscrapers"/>
+      <div className="card-body">
+        <h5 className="card-title">Cream Finder</h5>
+        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        <a className="btn btn-outline-dark btn-floating m-1" href="https://github.com/oconnorjohnson/cream-finder" role="button"
+        ><i className="fab fa-github"></i></a>
+        <a className="btn btn-outline-dark btn-floating m-1" href="https://enthousiaste-monsieur-80020.herokuapp.com/" role="button"
         ><iconify-icon inline icon="simple-icons:heroku"></iconify-icon></a>
       </div>
     </div>
   </div>
-  <div class="col">
-    <div class="card">
-      <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" class="card-img-top" alt="Skyscrapers"/>
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
+  <div className="col">
+    <div className="card">
+      <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/044.webp" className="card-img-top" alt="Skyscrapers"/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
           This is a longer card with supporting text below as a natural lead-in to
           additional content. This content is a little bit longer.
         </p>
