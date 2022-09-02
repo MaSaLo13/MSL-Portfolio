7e9b97d4d29e6639acc7e0ccac1315e25e59341e
marvinsantiago <marvinsantiago@MindYouBusiness.lan>
2022-09-01 23:13:16 -0700

    resume page, added skills

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
 
 
 export default function About() {
-  return ( <>
-    <div>
-      <h1>About Me</h1>
-      <p>
-      Full Stack web developer with a background in Political Science and International Studies. Recently earned a certificate in full stack development from the Coding Boot Camp at UCLA Extension; developed new skills that include: HTML, CSS,  JavaScript, BootStrap, API’s, Node, Express, MySQL, MVC Paradigm, Sequilieze, React, MongoDB, and MERN Stack. I applied HTML, CSS, handlebars.js Javascript and MySQL in a recent group project with 3 other people where we created a full stack website called Cream Finder that will help ice cream truck drivers with sharing their routes with users that enjoy ice cream. I am excited to use my newly developed skills in a fast-paced and driven team to build websites that users will enjoy.
+  return (
+<Container>
+<Row xs={1} md={2}>
+  <Col>
+  <h1>About Me</h1></Col>
+  <Col>
+  <img src="https://github.com/MaSaLo13/Weather-Dash/raw/main/assets/img/first.png" className="card-img-top" alt="Hollywood Sign on The Hill"/></Col>
+  <Col>
+  <p>
+Full Stack web developer with a background in Political Science and International Studies. Recently earned a certificate in full stack development from the Coding Boot Camp at UCLA Extension; developed new skills that include: HTML, CSS,  JavaScript, BootStrap, API’s, Node, Express, MySQL, MVC Paradigm, Sequilieze, React, MongoDB, and MERN Stack. I applied HTML, CSS, handlebars.js Javascript and MySQL in a recent group project with 3 other people where we created a full stack website called Cream Finder that will help ice cream truck drivers with sharing their routes with users that enjoy ice cream. I am excited to use my newly developed skills in a fast-paced and driven team to build websites that users will enjoy.
 
-      </p>
-    </div>
-    </>
+</p></Col>
+</Row>
+</Container>
+ 
+  
   );
 }
+
+
+
+
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,7 +1,47 @@
 import React from 'react';
 
 export default function Resume() {
-  return (<div class="resume">
+  return (
+    <section className="skill-section px-5" id="skill">
+    <div className="containers py-5">
+        <h4 >Skills </h4> 
+        <div className="row py-5">
+            <div className="col-md-5 d-flex justify-content-around py-2 flex-wrap">
+                <h3><iconify-icon icon="akar-icons:html-fill"></iconify-icon></h3>
+                <h3><iconify-icon icon="akar-icons:css-fill"></iconify-icon></h3>
+                <h3><iconify-icon icon="akar-icons:bootstrap-fill"></iconify-icon></h3>
+                <h3><iconify-icon icon="fa-brands:node"></iconify-icon></h3>
+                <h3><iconify-icon icon="akar-icons:javascript-fill"></iconify-icon></h3>
+                <h3><iconify-icon icon="akar-icons:jquery-fill"></iconify-icon></h3>
+                <h3><iconify-icon icon="cib:npm"></iconify-icon>
+</h3>
+            </div>
+        </div>         
+
+        <div className="row py-4">
+            <div className="col-md-5 d-flex justify-content-evenly p-2 flex-wrap">
+            <h3><iconify-icon icon="file-icons:sequelize"></iconify-icon></h3>
+                <h3><iconify-icon icon="logos:express"></iconify-icon></h3>
+                <h3><iconify-icon icon="vscode-icons:file-type-mongo"></iconify-icon></h3>
+                <h3><iconify-icon icon="logos:handlebars"></iconify-icon></h3>
+                <h3><iconify-icon icon="cib:mysql"></iconify-icon></h3>
+                <h3><iconify-icon icon="bxl:heroku"></iconify-icon></h3>
+                <h3><iconify-icon icon="akar-icons:react-fill"></iconify-icon></h3>
+                <h3><iconify-icon icon="simple-icons:apollographql"></iconify-icon>
+</h3>
+ 
+            </div>
+        </div>    
+
+    </div>       
+</section>
+  )
+}
+
+
+
+
+{/* <div class="resume">
       <h1>Resume</h1>
       <hr></hr>
       <p>
@@ -11,6 +51,8 @@ export default function Resume() {
     <iframe src="https://docs.google.com/document/d/1P170mLWOQWqgROO-WjSsZafwwHuSlCAeGzbrYl1MrXc/edit?usp=sharing" width="100%" height="500px">
     </iframe>
       
-    </div>
-  )
-}
+    </div> */}
+
+    // FRONT END: HTML, JAVASCRIPT, CSS, BOOTSTRAP, JQUERY
+    // NODE JS IS BOTH
+    //BACK END: MONGODB, MYSQL, 
