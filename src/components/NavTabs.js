6f205e0983605ca72ba1ba8ed3b373fd527983e4
marvinsantiago <marvinsantiago@MindYouBusiness.lan>
2022-09-06 22:44:19 -0700

    added collapsable navbar, not responsive though. changed aliceblue to white in css files. deleted image that was going to be used as background. added image of myself. index.html added jquery

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -5,39 +5,80 @@ import '../../src/App.css';
 // We assign them to their own variable names
 export default function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="navbar-brand">Marvin Santiago Lopez</li>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#about">Marvin Santiago Lopez</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <iconify-icon icon="akar-icons:three-line-horizontal"></iconify-icon>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About Me
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#projects"
-          onClick={() => handlePageChange('Projects')}
-          // Check to see if the currentPage is `Projects`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
-        >
-          Projects
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#resume"
-          onClick={() => handlePageChange('Resume')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-        >
-          Resume
-        </a>
-      </li>
-    </ul>
+  <a
+    href="#about"
+    onClick={() => handlePageChange('About')}
+    // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+  >
+    About Me
+  </a>
+</li>
+<li className="nav-item">
+  <a
+    href="#projects"
+    onClick={() => handlePageChange('Projects')}
+    // Check to see if the currentPage is `Projects`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+    className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+  >
+    Projects
+  </a>
+</li>
+<li className="nav-item">
+  <a
+    href="#resume"
+    onClick={() => handlePageChange('Resume')}
+    // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+  >
+    Resume
+  </a>
+</li>
+      </ul>
+    </div>
+  </nav>
   );
 }
 
+/* <ul className="nav nav-tabs">
+<li className="navbar-brand">Marvin Santiago Lopez</li>
+<li className="nav-item">
+  <a
+    href="#about"
+    onClick={() => handlePageChange('About')}
+    // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+  >
+    About Me
+  </a>
+</li>
+<li className="nav-item">
+  <a
+    href="#projects"
+    onClick={() => handlePageChange('Projects')}
+    // Check to see if the currentPage is `Projects`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+    className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+  >
+    Projects
+  </a>
+</li>
+<li className="nav-item">
+  <a
+    href="#resume"
+    onClick={() => handlePageChange('Resume')}
+    // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+  >
+    Resume
+  </a>
+</li>
+</ul> */
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,10 +10,12 @@ export default function About() {
 <Container>
 <Row xs={1} md={2}>
   <Col>
-  <h1>About Me</h1></Col>
+  </Col>
   <Col>
-  <img src="https://github.com/MaSaLo13/Weather-Dash/raw/main/assets/img/first.png" className="card-img-top" alt="Hollywood Sign on The Hill"/></Col>
+  <p>
+  I'm Marvin Santiago, a full-stack web developer based in Los Angeles.</p></Col>
   <Col>
+  <h1>About Me</h1>
   <p>
 Full Stack web developer with a background in Political Science and International Studies. Recently earned a certificate in full stack development from the Coding Boot Camp at UCLA Extension; developed new skills that include: HTML, CSS,  JavaScript, BootStrap, API’s, Node, Express, MySQL, MVC Paradigm, Sequilieze, React, MongoDB, and MERN Stack. I applied HTML, CSS, handlebars.js Javascript and MySQL in a recent group project with 3 other people where we created a full stack website called Cream Finder that will help ice cream truck drivers with sharing their routes with users that enjoy ice cream. I am excited to use my newly developed skills in a fast-paced and driven team to build websites that users will enjoy.
 
