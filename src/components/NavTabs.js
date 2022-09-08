c4bd91ee8777eddb3dd5142b4d32bb6cb795285c
marvinsantiago <marvinsantiago@MindYouBusiness.lan>
2022-09-07 23:34:18 -0700

    changed navbar color to medium purple. changed text in nav-item to white

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -5,14 +5,14 @@ import '../../src/App.css';
 // We assign them to their own variable names
 export default function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#about">Marvin Santiago Lopez</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <nav className="navbar navbar-expand-lg">
+      <a className="navbar-brand" href="#about">Marvin Santiago Lopez</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <iconify-icon icon="akar-icons:three-line-horizontal"></iconify-icon>
     </button>
   
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
       <li className="nav-item">
   <a
     href="#about"
@@ -43,9 +43,10 @@ export default function NavTabs({ currentPage, handlePageChange }) {
     Resume
   </a>
 </li>
+        
       </ul>
-    </div>
-  </nav>
+      </div>
+      </nav>
   );
 }
 
@@ -82,3 +83,45 @@ export default function NavTabs({ currentPage, handlePageChange }) {
   </a>
 </li>
 </ul> */
+
+/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#about">Marvin Santiago Lopez</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+    <iconify-icon icon="akar-icons:three-line-horizontal"></iconify-icon>
+    </button>
+  
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li className="nav-item">
+  <a
+    href="#about"
+    onClick={() => handlePageChange('About')}
+    // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+  >
+    About Me
+  </a>
+</li>
+<li className="nav-item">
+  <a
+    href="#projects"
+    onClick={() => handlePageChange('Projects')}
+    // Check to see if the currentPage is `Projects`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+    className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+  >
+    Projects
+  </a>
+</li>
+<li className="nav-item">
+  <a
+    href="#resume"
+    onClick={() => handlePageChange('Resume')}
+    // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+  >
+    Resume
+  </a>
+</li>
+      </ul>
+    </div>
+  </nav> */
